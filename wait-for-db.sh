793f84c38231182b57a4b2b9aa97ce494ef2fa40
Oussama0304 <oussama.boughanmi@esprit.tn>
2025-01-22 21:58:20 +0100

    nchallahtawa

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -6,7 +6,7 @@ host="$1"
 shift
 cmd="$@"
 
-until mysql -h "$host" -u root -pProjectPfeAgil -e 'SELECT 1'; do
+until mysql -h "$host" -u root -pProjectPfeAgil --ssl-mode=DISABLED -e 'SELECT 1'; do
   >&2 echo "MySQL is unavailable - sleeping"
   sleep 1
 done
